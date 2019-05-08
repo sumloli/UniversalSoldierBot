49091323cf47ad8776efe36db0a6ad10d359ea52
sumloli <sumloli@email.cz>
2019-05-08 15:43:46 +0200

    all is ok

--- bot_handlers.py
+++ bot_handlers.py
@@ -2,32 +2,36 @@ from bot import bot
 from messages import *
 
 
-@bot.message_handler(commands=['start', 'soldier'])
+#@bot.message_handler(commands=['start', 'soldier'])
+#def send_welcome(message):
+#    bot.send_message(message.chat.id, HELLO_MESSAGE)
+#    voice = open('/app/res/smeh.ogg', 'rb')
+#    bot.send_voice(message.chat.id, voice)
+
+@bot.message_handler(commands=['help', 'start'])
 def send_welcome(message):
-    bot.send_message(message.chat.id, HELLO_MESSAGE)
+    bot.reply_to(message, HELLO_MESSAGE)
+
+@bot.message_handler(commands=['смех'])
+def smeh(message):
+    bot.send_message(message.chat.id, 'доставлено:')
     voice = open('/app/res/smeh.ogg', 'rb')
     bot.send_voice(message.chat.id, voice)
 
-@bot.message_handler(commands=['help', 'start2'])
-def send_welcome2(message):
-    bot.reply_to(message, """\
-Hi there, I am EchoBot.
-I am here to echo your  kind words back to you. Just say anything nice and I'll say the exact same thing to you!\
-""")
-@bot.message_handler(regexp='(?:ору|лол)')
+@bot.message_handler(regexp='(?:ору|лол|смешно|хах|хаха)')
 def command_smeh(message):
     voice = open('/app/res/smeh.ogg', 'rb')
     bot.send_voice(message.chat.id, voice)
 
-@bot.message_handler(regexp='(?:смех|смешно)')
-def command_oldy3(message):
-    bot.send_message(message.chat.id, 'ниже смех')
-    voice = open('/app/res/smeh.ogg', 'rb')
-    bot.send_voice(message.chat.id, voice)
+#@bot.message_handler(regexp='(?:смех|смешно)')
+#def command_oldy3(message):
+#    bot.send_message(message.chat.id, 'ниже смех')
+#    voice = open('/app/res/smeh.ogg', 'rb')
+#    bot.send_voice(message.chat.id, voice)
 
-@bot.message_handler(content_types=['text'])
-def repeat_all_messages(message):
-    bot.send_message(message.chat.id, message.text)
+#@bot.message_handler(content_types=['text'])
+#def repeat_all_messages(message):
+#    bot.send_message(message.chat.id, message.text)
 
 
 if __name__ == '__main__':
