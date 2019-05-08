7b2260fb7d7513dda80ccde851a3bed38eae004f
sumloli <sumloli@email.cz>
2019-05-08 14:01:24 +0200

    all is ok

--- bot_handlers.py
+++ bot_handlers.py
@@ -5,9 +5,8 @@ from messages import *
 @bot.message_handler(commands=['start', 'soldier'])
 def send_welcome(message):
     bot.send_message(message.chat.id, HELLO_MESSAGE)
-    voice = open('/res/smeh.ogg', 'rb')
-    bot.send_voice(message, voice)
-    bot.send_voice(message, "FILEID")
+    voice = open('/Users/sumloli/PycharmProjects/UniversalSoldierBot/res/smeh.ogg', 'rb')
+    bot.send_voice(message.chat.id, voice)
 
 @bot.message_handler(commands=['help', 'start2'])
 def send_welcome2(message):
@@ -19,13 +18,13 @@ I am here to echo your kind words back to you. Just say anything nice and I'll s
 @bot.message_handler(regexp='че по олдам')
 def command_oldy(message):
     bot.send_message(message.chat.id, 'Сейчас проверим!')
-    bot.send_message(message.chat.id, 'Олды тут?')
+    bot.send_message(message.chat.id, 'Олды тут?тест')
 
 
-@bot.message_handler(regexp='че ')
+@bot.message_handler(regexp='(?:смех|смешно|лол)')
 def command_oldy3(message):
-    bot.send_message(message.chat.id, 'Сейчас проверим!')
-    bot.send_message(message.chat.id, 'Олды тут?')
+    voice = open('universalsoldierbot/res/smeh.ogg', 'rb')
+    bot.send_voice(message.chat.id, voice)
 
 @bot.message_handler(content_types=['text'])
 def repeat_all_messages(message):
--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-TOKEN = '805621916:AAHbTfy1aIEXtKo2uV-eaNKbxnT3xZeNHwM1'
+TOKEN = '805621916:AAFqqEf8dSFAJZiURXauiXJNHT4u-q0xZDc'
 APP_NAME = 'universalsoldierbot'
