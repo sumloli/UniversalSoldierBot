742adecc56a7bfac154a74b765ac99c82067dc06
sumloli <sumloli@email.cz>
2019-05-09 15:58:12 +0200

    Added cron process

--- clock.py
+++ clock.py
@@ -2,9 +2,11 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('interval', minutes=3)
+
+@sched.scheduled_job('interval', minutes=1)
 def timed_job():
     print('This job is run every three minutes.')
+
 
 @sched.scheduled_job('cron', day_of_week='mon-fri', hour=17)
 def scheduled_job():
