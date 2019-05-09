a496e3bac19404161273e54d8475e01e115735cf
sumloli <sumloli@email.cz>
2019-05-09 17:16:37 +0200

    changed cron message sending interval

--- clock.py
+++ clock.py
@@ -5,11 +5,11 @@ from countdown import *
 sched = BlockingScheduler()
 
 
-@sched.scheduled_job('interval', minutes=1)
-def timed_job():
-    bot.send_message(-294448452, text='This job is run every minute.')
-    bot.send_message(-294448452, sirenCountdown())
-    print('This job is run every minute.')
+#@sched.scheduled_job('interval', minutes=1)
+#def timed_job():
+#    bot.send_message(-294448452, text='This job is run every minute.')
+#    bot.send_message(-294448452, sirenCountdown())
+#    print('This job is run every minute.')
 
 
 @sched.scheduled_job('cron', day='*', hour=10)
