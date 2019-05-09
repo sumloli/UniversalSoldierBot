11ee9b6bb8b418b716059b28c0ce4f7e369225cf
sumloli <sumloli@email.cz>
2019-05-09 17:07:58 +0200

    added cron message sending

--- clock.py
+++ clock.py
@@ -11,7 +11,7 @@ def timed_job():
     print('This job is run every minute.')
 
 
-@sched.scheduled_job('cron', day='*', hour=15, minute=5)
+@sched.scheduled_job('cron', day='*', hour=15, minute=9)
 def scheduled_job():
     bot.send_message(-266154989, sirenCountdown())
     print('This job is run every day at 12.')
