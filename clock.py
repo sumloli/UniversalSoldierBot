22bbb8e9b67f1212565f1b1a68f7956a6c2ba21a
sumloli <sumloli@email.cz>
2019-05-09 16:10:35 +0200

    added cron message sending

--- clock.py
+++ clock.py
@@ -1,15 +1,20 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
+from bot import bot
+from countdown import *
 
 sched = BlockingScheduler()
 
 
 @sched.scheduled_job('interval', minutes=1)
 def timed_job():
-    print('This job is run every three minutes.')
+    @bot.message_handler(commands=['siren'])
+    def send_help(message):
+        bot.reply_to(message, sirenCountdown())
+    print('This job is run every minute.')
 
 
-@sched.scheduled_job('cron', day_of_week='mon-fri', hour=17)
-def scheduled_job():
-    print('This job is run every weekday at 5pm.')
+#@sched.scheduled_job('cron', day_of_week='mon-fri', hour=17)
+#def scheduled_job():
+#    print('This job is run every weekday at 5pm.')
 
 sched.start()
