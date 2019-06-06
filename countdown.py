01af25822fbc31a36b49e35a9b10d73f2737c2bb
sumloli <46442842+sumloli@users.noreply.github.com>
2019-06-06 15:15:24 +0200

    Update countdown.py

--- countdown.py
+++ countdown.py
@@ -7,7 +7,8 @@ def sirenCountdown():
     datelist = list(rrule(MONTHLY, count=10, byweekday=WE(1), dtstart=parse("2019-05-01T11:59:59")))
     dt = datetime.datetime
     now = dt.now()
-    count = datelist[1] - dt(year=now.year, month=now.month, day=now.day,
-                             hour=now.hour+2, minute=now.minute, second=now.second)
-
-    return 'До сирены осталось: {}'.format(count)
+    for nextsiren in range(len(datelist)):
+        while now < datelist[nextsiren]:
+            count = datelist[nextsiren] - dt(year=now.year, month=now.month, day=now.day,
+                                             hour=now.hour, minute=now.minute, second=now.second)
+            return 'До сирены осталось: {}'.format(count)
