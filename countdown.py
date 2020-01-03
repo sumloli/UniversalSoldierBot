61804bd8c5b19afecb6c5f8ed91e6c0cb1ecc5f3
sumloli <sumloli@email.cz>
2020-01-03 08:47:21 +0100

    winter time change

--- countdown.py
+++ countdown.py
@@ -10,5 +10,5 @@ def sirenCountdown():
     for nextsiren in range(len(datelist)):
         while now < datelist[nextsiren]:
             count = datelist[nextsiren] - dt(year=now.year, month=now.month, day=now.day,
-                                             hour=now.hour+2, minute=now.minute, second=now.second)
+                                             hour=now.hour+1, minute=now.minute, second=now.second)
             return 'До сирены осталось: {}'.format(count)
