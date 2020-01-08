1bfc0211333949277fcb04d2a504545dff2b1225
sumloli <sumloli@email.cz>
2020-01-08 10:49:18 +0100

    dialogflow temp try 6

--- bot_handlers.py
+++ bot_handlers.py
@@ -75,7 +75,7 @@ def smeh(message):
 @bot.message_handler(func=lambda message: True, content_types=['text'])
 def text_message(message):
     print(message)
-    if message.chat.type == "private" or message.chat.id == 805621916:
+    if message.chat.type == "private" or message.reply_to_message.from_user.id == 805621916:
         request = apiai.ApiAI(config.DF_TOKEN).text_request()  # Token API of Dialogflow
         request.lang = config.BOT_LANG  # lang of request
         request.session_id = config.DF_SESSION  # ID of dialog session (for bot learning)
