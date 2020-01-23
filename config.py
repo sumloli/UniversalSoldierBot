cf075fa333df68b5056bef1aeda2683b36a662ef
sumloli <sumloli@email.cz>
2020-01-23 16:54:55 +0100

    removing config

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-
-APP_NAME = 'universalsoldierbot'
-TG_TOKEN = os.environ['TG_TOKEN']
-DF_TOKEN = os.environ['DF_TOKEN']
-BOT_LANG = 'ru'
-DF_SESSION = '123456789'
-DB_PASS = os.environ['DB_PASS']
