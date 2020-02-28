12f189aa5f1a836ba9d161badbe279def739f6e8
sumloli <sumloli@email.cz>
2020-02-28 09:58:17 +0100

    siren fix

--- calculations.py
+++ calculations.py
@@ -20,4 +20,3 @@ def siren_countdown():
             count = datelist[nextsiren] - dt(year=now.year, month=now.month, day=now.day,
                                              hour=now.hour+1, minute=now.minute, second=now.second)
             return 'До сирены осталось: {}'.format(count+datetime.timedelta(0, 1))
-print(siren_countdown())
