c6b3ced7c77246c8f9ed12cb492c58260384f6ad
sumloli <sumloli@email.cz>
2020-09-09 08:42:50 +0200

    siren time fix debug mode

--- clock.py
+++ clock.py
@@ -13,21 +13,21 @@ sched = BlockingScheduler()
 #     print('This job is run every minute.')
 
 
-# @sched.scheduled_job('interval', minutes=1)
-# def scheduled_job():
-#     bot.send_message(-294448452, text='DEBUG')
-#     bot.send_message(-294448452, text='This job is run every minute')
-#     bot.send_message(-294448452, siren_countdown().split(',')[0])
-#     print('This job is run every minute.')
+@sched.scheduled_job('interval', minutes=1)
+def scheduled_job():
+    bot.send_message(-294448452, text='DEBUG')
+    bot.send_message(-294448452, text='This job is run every minute')
+    bot.send_message(-294448452, siren_countdown().split(',')[0])
+    print('This job is run every minute.')
 
 
-@sched.scheduled_job('cron', day='*', hour=11)
+@sched.scheduled_job('cron', day='*', hour=10)
 def scheduled_job():
     bot.send_message(-294448452, siren_countdown().split(',')[0])
     print('This job is run every day at 12.')
 
 
-@sched.scheduled_job('cron', day='*', hour=10)
+@sched.scheduled_job('cron', day='*', hour=9)
 def scheduled_job():
     bot.send_message(-294448452, f'Ежедневное напоминание что Саня - {db_sanya()}')
     print('This job is run every day at 11.')
